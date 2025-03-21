d217742a3d3a54d491e926b41ba2ec1368c656d1
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-21 14:48:38 +0100

    added nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text className="text-2xl text-blue-500 text-center">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
