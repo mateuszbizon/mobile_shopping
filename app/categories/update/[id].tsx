2e33f63655fb72d179b007d17f9915bd09915f78
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-26 18:06:46 +0100

    added loading and not found message to category

--- app/categories/update/[id].tsx
+++ app/categories/update/[id].tsx
@@ -5,22 +5,31 @@ import { useAuth } from '@/context/AuthContext'
 import { getCategory } from '@/services/categoryService'
 import { Models } from 'react-native-appwrite'
 import CategoryForm from '@/components/forms/CategoryForm'
+import PageLoading from '@/components/PageLoading'
+import NotFound from '@/components/NotFound'
 
 const update = () => {
     const { id } = useLocalSearchParams<{ id: string }>()
     const { user } = useAuth()
     const [category, setCategory] = useState<Models.Document | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         if (user) {
             const fetchCategory = async () => {
+                setIsLoading(true)
                 const category = await getCategory(id)
                 setCategory(category)
+                setIsLoading(false)
             }
 
             fetchCategory()
         }
     }, [id, user])
+
+    if (isLoading) return <PageLoading />
+
+    if (!category) return <NotFound text='Produkt którego szukasz nie został znaleziony' />
 
   return (
     <View className='container'>
