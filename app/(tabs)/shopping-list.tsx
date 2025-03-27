3816f3a0e624137f725bfc70f9cbba235cf1a5b2
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-27 15:13:51 +0100

    fetching all needed products

--- app/(tabs)/shopping-list.tsx
+++ app/(tabs)/shopping-list.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '@/context/AuthContext'
+import { getShoppingList } from '@/services/shoppingListService'
+import { getUserProducts } from '@/services/productService'
+import { Models } from 'react-native-appwrite'
 
 const shoppingList = () => {
+    const { user } = useAuth()
+    const [shoppingList, setShoppingList] = useState<Models.Document[]>([]);
+    const [userProducts, setUserProducts] = useState<Models.Document[]>([]);
+    const [availableProducts, setAvailableProducts] = useState<Models.Document[]>([]);
+
+    useEffect(() => {
+        if (user) {
+            fetchData()
+        }
+    }, [user])
+
+    const fetchData = async () => {
+        const shoppingListData = await getShoppingList(user?.id!)
+        const userProductsData = await getUserProducts(user?.id!)
+
+        const shoppingListIds = shoppingListData.map(item => item.products.$id)
+
+        const availableProducts = userProductsData.filter(item => !shoppingListIds.includes(item.$id))
+
+        setShoppingList(shoppingListData)
+        setUserProducts(userProductsData)
+        setAvailableProducts(availableProducts)
+    }
+
   return (
     <View className='container'>
-        <Text>shoppingList</Text>
+        <FlatList
+            data={availableProducts}
+            keyExtractor={(item) => item.$id}
+            renderItem={({ item }) => (
+                <Text>{item.name}</Text>
+            )}
+        />
     </View>
   )
 }
