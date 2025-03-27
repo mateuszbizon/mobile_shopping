91330378215266d11f39eec5c4c9895b6c452143
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-27 12:26:42 +0100

    added shopping list collection id in appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -11,3 +11,4 @@ export const account = new Account(client)
 export const DATABASE_ID = "67e11a9c0014ad71a818"
 export const CATEGORIES_ID = "67e11aaa00141b675fb0"
 export const PRODUCTS_ID = "67e13593001f84146b05"
+export const SHOPPING_LIST_ID = "67e5329c0036b3e80be1"
