ca8e700bd8a0ef0010e7aa9cd5e670c6b10af93d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-31 11:13:47 +0200

    added search products for available products in shopping list

--- components/ShoppingListAddProducts.tsx
+++ components/ShoppingListAddProducts.tsx
@@ -1,9 +1,10 @@
 import { View, Text, Modal, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Models } from 'react-native-appwrite'
 import { MaterialIcons } from '@expo/vector-icons'
 import AddProductCard from './cards/AddProductCard'
 import EmptyList from './EmptyList'
+import SearchProducts from './SearchProducts'
 
 type ShoppingListAddProductsProps = {
     modalVisible: boolean
@@ -14,15 +15,22 @@ type ShoppingListAddProductsProps = {
 }
 
 const ShoppingListAddProducts = ({ modalVisible, products, onClose, setProducts, setShoppingList }: ShoppingListAddProductsProps) => {
+const [searchedProducts, setSearchedProducts] = useState<Models.Document[]>([])
+
     function deleteAvailableProduct(productId: string, shoppingListProduct: Models.Document) {
         setProducts(products.filter(product => product.$id !== productId))
         setShoppingList(prev => [shoppingListProduct, ...prev])
     }
 
+    useEffect(() => {
+        setSearchedProducts([...products])
+    }, [products])
+
   return (
     <Modal animationType="slide" transparent={true} visible={modalVisible}>
         <View className='p-4 bg-white rounded-lg shadow-md flex-1'>
             <Text className="heading2 text-center mb-8">Wybierz produkty</Text>
+            <SearchProducts products={products} setSearchProducts={setSearchedProducts} />
             <TouchableOpacity onPress={onClose} className='mb-8'>
                 <Text>
                     <View className='flex flex-row items-center gap-3'>
@@ -32,7 +40,7 @@ const ShoppingListAddProducts = ({ modalVisible, products, onClose, setProducts,
                 </Text>
             </TouchableOpacity>
             <FlatList
-                data={products}
+                data={searchedProducts}
                 keyExtractor={(item) => item.$id}
                 renderItem={({ item }) => (
                     <AddProductCard key={item.$id} product={item} deleteAvailableProduct={deleteAvailableProduct} />
